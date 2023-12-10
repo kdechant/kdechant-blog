057cac1319d7198e6c8bf282a5ea4de47eafc80b
Keith Dechant <keith.dechant@gmail.com>
2023-12-09 23:58:13 -0800

    Fix Google Analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -25,12 +25,9 @@ const siteMetadata = {
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
-    // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    posthogProjectApiKey: '', // e.g. AhnJK8392ndPOav87as450xd
-    googleAnalyticsId: '370976622', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalytics: {
+      googleAnalyticsId: 'G-DHZZT0PDY7',
+    }
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
